85bb108cffa7a2499d1c42f0a4c1d12c2e444199
Luke Finch <luke.finch@news.co.uk>
2018-04-04 17:41:57 +0100

    Setting up for publishing to SKPM

--- src/my-command.js
+++ src/my-command.js
@@ -1,3 +1,61 @@
-export default function(context) {
-  context.document.showMessage("It's alive 🙌")
+sketch = context.api()
+doc = context.document;
+sel = context.selection;
+currentPage = doc.currentPage();
+font = [];
+colours = [];
+
+function addArtboard(page, name) {
+    var artboard = MSArtboardGroup.new();
+    frame = artboard.frame();
+    frame.setWidth(4000);
+    frame.setHeight(4000);
+    frame.setConstrainProportions(false);
+    page.addLayers([artboard]);
+    artboard.setName(name);
+    return artboard;
 }
+
+if(sel == 0){doc.showMessage('You need to select some text and colours!')}
+else{
+var board = new addArtboard(currentPage, "Quick Text Styles")
+
+function addTextLayer (target, label, font, colour,i, j) {
+    var textLayer = MSTextLayer.new();
+    console.log(textLayer);
+    textLayer.setStringValue("The Sun, example text")
+    textLayer.setName(label)
+    textLayer.setFont(font)
+    textLayer.setTextColor(colour)
+    textLayer.setTextBehaviour(1)
+    textLayer.absoluteRect().setWidth(500)
+    textLayer.absoluteRect().setHeight(200)
+    textLayer.absoluteRect().setRulerX(i*600)
+    textLayer.absoluteRect().setRulerY(j*(textLayer.frame().height()+50))
+    target.addLayers([textLayer]);
+    return textLayer;
+  };
+
+context.api().selectedDocument.selectedLayers.iterate(layer => {
+
+if(layer['_object'].isKindOfClass(MSTextLayer)){
+            font.push({
+            "font": layer['_object'].font(),
+            "text": layer['_object'].name(),
+            }
+            )}
+if(layer['_object'].isKindOfClass(MSShapeGroup)){
+            colours.push({
+                "colour":layer['_object'].style().fills()[0].color(),
+                "name":layer['_object'].name(),
+                }
+            )}
+})
+
+for(var i = 0; i < colours.length; i++){
+for(var j = 0; j < font.length; j++){
+addTextLayer(board, font[j].text + "/" + colours[i].name, font[j].font, colours[i].colour, i, j)
+}}
+board.resizeToFitChildren();
+
+} //end of else
